986e1fd4e9450b2dbef30010cda6dba9c322599f
Tiago Tresoldi <tresoldi@shh.mpg.de>
2021-03-02 16:10:53 +0100

    Add entropy normalized compression distance method

--- src/seqsim/compression.py
+++ src/seqsim/compression.py
@@ -14,15 +14,12 @@ import logging
 import textdistance
 
 # TODO: multiple sequences?
+# TODO: consider splitting in similarity/distance
 def arith_ncd(
     seq_x: Sequence[Hashable], seq_y: Sequence[Hashable], normal: bool = False
 ) -> float:
     """
     Computes a distance between two sequences based on Arithmetic Coding.
-
-    The function accepts the `normal` parameter to have calls equivalent to those
-    of other methods, but it is redundant as the Arithmetic Coding normalized
-    compression distance is already in range [0..1].
 
     See: https://en.wikipedia.org/wiki/Arithmetic_coding
 
@@ -37,9 +34,35 @@ def arith_ncd(
     seq_x = "".join([str(elem) for elem in seq_x])
     seq_y = "".join([str(elem) for elem in seq_y])
 
+    # TODO: have a normalization method based on seq_x*2 and seq_y*2
     if normal:
-        logging.warning(
-            "Arithmetic Coding NCD is always in [0..1] range, no need for `normal` parameter."
-        )
+        logging.warning("Arithmetic Coding NCD cannot be normalized in range [0..1].")
 
-    return 1.0 - textdistance.arith_ncd(seq_x, seq_y)
+    return textdistance.arith_ncd(seq_x, seq_y)
+
+
+# TODO: multiple sequences?
+def entropy_ncd(
+    seq_x: Sequence[Hashable], seq_y: Sequence[Hashable], normal: bool = False
+) -> float:
+    """
+    Computes a distance between two sequences based on entropy.
+
+    See: https://en.wikipedia.org/wiki/Entropy_(information_theory)
+
+    @param seq_x: The first sequence to be compared.
+    @param seq_y: The second sequence to be compared.
+    @param normal: Dummy parameter, see comment above.
+    @return: The Entropy NCD between the two sequences.
+    """
+
+    # As the method uses .find, we need strings
+    # TODO: rewrite proper solution generalizing for all sequences
+    seq_x = "".join([str(elem) for elem in seq_x])
+    seq_y = "".join([str(elem) for elem in seq_y])
+
+    # TODO: have a normalization method based on seq_x*2 and seq_y*2
+    if normal:
+        logging.warning("Entropy NCD cannot be normalized in range [0..1].")
+
+    return textdistance.entropy_ncd(seq_x, seq_y)
--- tests/test_compression.py
+++ tests/test_compression.py
@@ -15,10 +15,10 @@ from seqsim import compression
 @pytest.mark.parametrize(
     "seq_x,seq_y,expected,tol",
     [
-        ["kitten", "sitting", -0.25, 0.0],
-        [(1, 2, 3), [1, 2, 3], -0.25, 0.0],
-        [(1, 2, 3, 4, 5), (1, 2, 4, 3, 6, 7), 0.0, 0.0],
-        [(1, 2, 3), ["a", "b", "c", "d"], -0.555555, 1e-6],
+        ["kitten", "sitting", 1.25, 0.0],
+        [(1, 2, 3), [1, 2, 3], 1.25, 0.0],
+        [(1, 2, 3, 4, 5), (1, 2, 4, 3, 6, 7), 1.0, 0.0],
+        [(1, 2, 3), ["a", "b", "c", "d"], 1.555555, 1e-6],
     ],
 )
 def test_arith_ncd(seq_x, seq_y, expected, tol):
@@ -27,3 +27,22 @@ def test_arith_ncd(seq_x, seq_y, expected, tol):
 
     # Test symmetry
     assert compression.arith_ncd(seq_x, seq_y) == compression.arith_ncd(seq_y, seq_x)
+
+
+@pytest.mark.parametrize(
+    "seq_x,seq_y,expected,tol",
+    [
+        ["kitten", "sitting", 0.101340, 1e-6],
+        [(1, 2, 3), [1, 2, 3], 0.0, 0.0],
+        [(1, 2, 3, 4, 5), (1, 2, 4, 3, 6, 7), 0.114430, 1e-6],
+        [(1, 2, 3), ["a", "b", "c", "d"], 0.407464, 1e-6],
+    ],
+)
+def test_entropy_ncd(seq_x, seq_y, expected, tol):
+    # Test hard-coded expected value
+    assert compression.entropy_ncd(seq_x, seq_y) == pytest.approx(expected, abs=tol)
+
+    # Test symmetry
+    assert compression.entropy_ncd(seq_x, seq_y) == compression.entropy_ncd(
+        seq_y, seq_x
+    )
